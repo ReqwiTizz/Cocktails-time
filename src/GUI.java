1e35151ca37043356a54e3c46d9f6f89471e8272
ReqwiTizz <reqwitizz@gmail.com>
2020-09-20 20:07:21 +0300

    added JTable,JTextField,JButton

--- src/GUI.java
+++ src/GUI.java
@@ -4,18 +4,30 @@ import java.awt.*;
 public class GUI {
     private JPanel panel;
     private JFrame frame;
-
-
+    private JTextField text;
+    private JButton button;
+    private JTable table1, table2;
+    private Object[][] array = new String[][] {{"Дайкири", "Ром" , "ml.", "40"},
+                                                {"-", "Лимонный сок", "ml."  , "20"},
+                                                {"-", "Сахарный сироп", "ml.", "10"}};
+    private Object[] collumsHeader = new String[] {"Название", "Состовляющие", "Ед.измерения", "Количество"};
     public GUI() {
         frame = new JFrame();
-
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        button = new JButton("Search");
+        text = new JTextField("Поиск", 20);
+        table1 = new JTable(array, collumsHeader);
+
+
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 50, 100));
+        panel.setLayout(new BorderLayout());
+        panel.add(button, BorderLayout.WEST);
+        panel.add(text);
+        panel.add(table1, BorderLayout.EAST);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Coctails Choice");
+        frame.setTitle("Cocktails Choice");
         frame.pack();
         frame.setVisible(true);
 
